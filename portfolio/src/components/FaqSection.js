28ca4f55d3ff1d6738c5964163ef46c29f7e0218
Thomas Maneschijn <thomasmaneschijn@gmail.com>
2021-02-11 15:53:02 +0100

    Faq section

---
+++ portfolio/src/components/FaqSection.js
@@ -0,0 +1,37 @@
+const FaqSection = () => {
+    return(
+        <div className="faq">
+            <h2>Any Questions <span>FAQ</span></h2>
+            <div className="question">
+                <h4>How Do I Start?</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus quis minima quisquam sed voluptas nisi obcaecati sunt error tempora architecto inventore, libero repellendus incidunt omnis, reprehenderit ullam cumque doloremque deleniti.</p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>Daily schedule</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus quis minima quisquam sed voluptas nisi obcaecati sunt error tempora architecto inventore, libero repellendus incidunt omnis, reprehenderit ullam cumque doloremque deleniti.</p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>Different payment methods</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus quis minima quisquam sed voluptas nisi obcaecati sunt error tempora architecto inventore, libero repellendus incidunt omnis, reprehenderit ullam cumque doloremque deleniti.</p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>What products do you offer?</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus quis minima quisquam sed voluptas nisi obcaecati sunt error tempora architecto inventore, libero repellendus incidunt omnis, reprehenderit ullam cumque doloremque deleniti.</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default FaqSection;
